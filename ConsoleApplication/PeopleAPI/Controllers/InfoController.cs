9584cea099f4f0781a7aa7d19e3979697f884fa7
zyy <1132012305@qq.com>
2018-11-29 17:28:33 +0800

    11月29号微信功能测试

--- ConsoleApplication/PeopleAPI/Controllers/InfoController.cs
+++ ConsoleApplication/PeopleAPI/Controllers/InfoController.cs
@@ -10,19 +10,16 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Http.Results;
 using System.Web.Security;
+using MySql.Data.MySqlClient;
+using System.Xml;
 
 namespace PeopleAPI.Controllers
 {
     public class InfoController : ApiController
     {
-        /// <summary>
-        /// 接口认证
-        /// </summary>
-        /// <param name="echostr"></param>
-        /// <param name="signature"></param>
-        /// <param name="timestamp"></param>
-        /// <param name="nonce"></param>
-        /// <returns></returns>
+        private static string connstr = "server=  127.0.0.1  ;  port=  3309  ;  user id=  root  ;  password=  evEt2ylnL!1201  ;  database=  wechat  ;  pooling=true";
+        #region 微信接口认证
+        // 接口认证
         [HttpGet]
         public async Task<HttpResponseMessage> wx(string echostr, string signature, string timestamp, string nonce)
         {
@@ -33,22 +30,8 @@ namespace PeopleAPI.Controllers
 
             return responseMessage;
         }
-        /// <summary>
-        /// 接收客服消息
-        /// </summary>
-        /// <returns></returns>
-        //[HttpPost]
-        //public async Task<HttpResponseMessage> wx()
-        //{
-        //    LogHelper log = new LogHelper();
-        //    var content = await Request.Content.ReadAsStringAsync();
-        //    log.LogError(content);
-        //    HttpResponseMessage responseMessage = new HttpResponseMessage();
-        //    return responseMessage;
-        //}
-        /// <summary>
-        /// 验证微信签名
-        /// </summary>
+      
+        // 验证微信签名
         private bool CheckSignature(string token, string signature, string timestamp, string nonce)
         {
             string[] ArrTmp = { token, timestamp, nonce };
@@ -73,51 +56,88 @@ namespace PeopleAPI.Controllers
                 return false;
             }
         }
-        //public HttpResponseMessage Get(string signature, string timestamp, string nonce, string echostr)
-        //{
-        //    //公众平台上开发者设置的token, appID, EncodingAESKey
-        //    string sToken = "zyy";
-        //    string sAppID = "wx5b3d448e0e1cad7c";
-        //    string sEncodingAESKey = "gFLPaPZbUHRI1McPvOT1lDkNhxsyIVhrP8KNxSRBLWF";
+        #endregion
+    
+        // 接收客户消息  自动回复
+        [HttpPost]
+        public async Task<HttpResponseMessage> wx()
+        {
+            HttpResponseMessage result = new HttpResponseMessage { Content = new StringContent("", Encoding.GetEncoding("UTF-8"), "text/plain") };
+            string contentXML = await Request.Content.ReadAsStringAsync();
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(contentXML);
+                string toUserName = xmlDoc.SelectSingleNode("xml/ToUserName").InnerText;
+                string fromUserName = xmlDoc.SelectSingleNode("xml/FromUserName").InnerText;
+                string createTime = xmlDoc.SelectSingleNode("xml/CreateTime").InnerText;
+                string msgType = xmlDoc.SelectSingleNode("xml/MsgType").InnerText;
+                string content = xmlDoc.SelectSingleNode("xml/Content").InnerText;
+                string msgId = xmlDoc.SelectSingleNode("xml/MsgId").InnerText;
 
-        //    Tencent.WXBizMsgCrypt wxcpt = new Tencent.WXBizMsgCrypt(sToken, sEncodingAESKey, sAppID);
+                using (MySqlConnection conn = new MySqlConnection(connstr))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = @"insert into content 
+(ToUserName, FromUserName, CreateTime, MsgType, Content, MsgId) values 
+(@ToUserName, @FromUserName, @CreateTime, @MsgType, @Content, @MsgId);";
+                    cmd.Parameters.AddWithValue("@ToUserName", toUserName);
+                    cmd.Parameters.AddWithValue("@FromUserName", fromUserName);
+                    cmd.Parameters.AddWithValue("@CreateTime", createTime);
+                    cmd.Parameters.AddWithValue("@MsgType", msgType);
+                    cmd.Parameters.AddWithValue("@Content", content);
+                    cmd.Parameters.AddWithValue("@MsgId", msgId);
+                    cmd.ExecuteNonQuery();
 
-        //    /* 1. 对用户回复的数据进行解密。
-        //    * 用户回复消息或者点击事件响应时，企业会收到回调消息，假设企业收到的推送消息：
-        //    * 	POST /cgi-bin/wxpush? msg_signature=477715d11cdb4164915debcba66cb864d751f3e6&timestamp=1409659813&nonce=1372623149 HTTP/1.1
-        //       Host: qy.weixin.qq.com
-        //       Content-Length: 613
-        //    *
-        //    * 	<xml>
-        //           <ToUserName><![CDATA[wx5823bf96d3bd56c7]]></ToUserName>
-        //           <Encrypt><![CDATA[RypEvHKD8QQKFhvQ6QleEB4J58tiPdvo+rtK1I9qca6aM/wvqnLSV5zEPeusUiX5L5X/0lWfrf0QADHHhGd3QczcdCUpj911L3vg3W/sYYvuJTs3TUUkSUXxaccAS0qhxchrRYt66wiSpGLYL42aM6A8dTT+6k4aSknmPj48kzJs8qLjvd4Xgpue06DOdnLxAUHzM6+kDZ+HMZfJYuR+LtwGc2hgf5gsijff0ekUNXZiqATP7PF5mZxZ3Izoun1s4zG4LUMnvw2r+KqCKIw+3IQH03v+BCA9nMELNqbSf6tiWSrXJB3LAVGUcallcrw8V2t9EL4EhzJWrQUax5wLVMNS0+rUPA3k22Ncx4XXZS9o0MBH27Bo6BpNelZpS+/uh9KsNlY6bHCmJU9p8g7m3fVKn28H3KDYA5Pl/T8Z1ptDAVe0lXdQ2YoyyH2uyPIGHBZZIs2pDBS8R07+qN+E7Q==]]></Encrypt>
-        //       </xml>
-        //    */
-        //    string sMsg = "";  //解析之后的明文
-        //    int ret = 0;
-        //    ret = wxcpt.DecryptMsg(signature, timestamp, nonce, echostr, ref sMsg);
-        //    if (ret != 0)
-        //    {
-        //        return null;
-        //    }
-        //    return new HttpResponseMessage()
-        //    {
-        //        Content = new StringContent(sMsg, Encoding.GetEncoding("UTF-8"), "application/x-www-form-urlencoded")
-        //    };
-        //}
+                    MySqlCommand cmd2 = conn.CreateCommand();
+                    cmd2.CommandText = @"insert into xmlcontent (XmlContent) values (@XmlContent);";
+                    cmd2.Parameters.AddWithValue("@XmlContent", contentXML);
+                    cmd2.ExecuteNonQuery();
+                }
+                DateTime dt1970 = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                long timeticks = (DateTime.Now.Ticks - dt1970.Ticks) / 10000000;
+                //返回纯文本text/plain  ,返回json application/json  ,返回xml text/xml
+//                string returnXml = string.Format(@"<xml> <ToUserName>< ![CDATA[{0}] ></ToUserName> 
+//<FromUserName>< ![CDATA[{1}] ></FromUserName> 
+//<CreateTime>{2}</CreateTime> 
+//<MsgType>< ![CDATA[text] ></MsgType> 
+//<Content>< ![CDATA[你好] ></Content> 
+//</xml>", fromUserName, toUserName, timeticks);
+                string returnXml = string.Format(@"<xml> <ToUserName>{0}</ToUserName> 
+<FromUserName>{1}</FromUserName> 
+<CreateTime>{2}</CreateTime> 
+<MsgType>text</MsgType> 
+<Content>你好</Content> 
+</xml>", fromUserName, toUserName, timeticks);
+                result = new HttpResponseMessage { Content = new StringContent(returnXml, Encoding.GetEncoding("UTF-8"), "text/xml") };
+                return result;
+            }
+            catch (Exception err)
+            {
+                MysqlLog(err.Message);
+            }
+            return result;
+        }
+     
+        // 填写错误日志
+        private void MysqlLog(string str)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connstr))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = @"insert into log (Content, LogTime) values (@Content, NOW());";
+                    cmd.Parameters.AddWithValue("@Content", str);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
 
-        //public HttpResponseMessage Get(string signature, string timestamp, string nonce, string echostr)
-        //{
-        //    string[] ArrTmp = { "zyy", timestamp, nonce };
-        //    Array.Sort(ArrTmp);
-        //    string tmpStr = string.Join("", ArrTmp);
-        //    var result = FormsAuthentication.HashPasswordForStoringInConfigFile(tmpStr, "SHA1").ToLower();
-
-        //    return new HttpResponseMessage()
-        //    {
-        //        Content = new StringContent(result, Encoding.GetEncoding("UTF-8"), "application/x-www-form-urlencoded")
-        //    };
-        //}
-
+            }
+        }
     }
 }
