49c48be3bdf760b4761a4ac2e3df8e47f7bd8435
1111chance <1132012305@qq.com>
2019-03-21 14:13:09 +0800

    增加backgroundworker

--- Monte Carlo method/Monte Carlo method/Form1.Designer.cs
+++ Monte Carlo method/Monte Carlo method/Form1.Designer.cs
@@ -29,7 +29,9 @@
         private void InitializeComponent()
         {
             this.btnStart = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bgw1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // btnStart
@@ -42,19 +44,36 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
-            // textBox1
+            // tb1
             // 
-            this.textBox1.Location = new System.Drawing.Point(197, 68);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(241, 29);
-            this.textBox1.TabIndex = 1;
+            this.tb1.Location = new System.Drawing.Point(317, 68);
+            this.tb1.Name = "tb1";
+            this.tb1.ReadOnly = true;
+            this.tb1.Size = new System.Drawing.Size(241, 29);
+            this.tb1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "进度：0";
+            // 
+            // bgw1
+            // 
+            this.bgw1.WorkerReportsProgress = true;
+            this.bgw1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw1_DoWork);
+            this.bgw1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw1_ProgressChanged);
+            this.bgw1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw1_RunWorkerCompleted);
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(519, 158);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(633, 166);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb1);
             this.Controls.Add(this.btnStart);
             this.Font = new System.Drawing.Font("微软雅黑 Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -71,7 +90,9 @@
         #endregion
 
         private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker bgw1;
     }
 }
 
--- Monte Carlo method/Monte Carlo method/Form1.cs
+++ Monte Carlo method/Monte Carlo method/Form1.cs
@@ -19,12 +19,22 @@ namespace Monte_Carlo_method
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            double N = 1000000000;
+            bgw1.RunWorkerAsync();
+        }
+
+        private void bgw1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            double N = Math.Pow(10, 8);
+            double reportStep = N / 100;
             double count = 0;
             Random rnd = new Random();
             double x, y;
             for (int i = 0; i < N; i++)
             {
+                if ((i % reportStep) == 0)
+                {
+                    bgw1.ReportProgress((int)(i / reportStep));
+                }
                 x = rnd.NextDouble();
                 y = rnd.NextDouble();
                 if ((x * x + y * y) < 1.0d)
@@ -33,7 +43,18 @@ namespace Monte_Carlo_method
                 }
             }
             double pi = 4 * count / N;
-            textBox1.Text = pi.ToString();
+            e.Result = pi.ToString();
+        }
+
+        private void bgw1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label1.Text = "进度：" + e.ProgressPercentage.ToString() + " %";
+        }
+
+        private void bgw1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            label1.Text = "进度：100%";
+            tb1.Text = e.Result as string;
         }
     }
 }
