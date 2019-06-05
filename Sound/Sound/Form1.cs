0728c2391cda87cdf13597380276d07308115cb0
1111chance <1132012305@qq.com>
2019-06-05 21:25:42 +0800

    sound

---
+++ Sound/Sound/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sound
+{
+    public partial class Form1 : Form
+    {
+        private SoundRecord recorder = new SoundRecord();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            recorder = new SoundRecord();
+            recorder.SetFileName("2.mp3");
+            recorder.RecStart();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            recorder.RecStop();
+            recorder = null;
+        }
+    }
+}
